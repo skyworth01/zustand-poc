a8c5508eb67fa48adc22b829149d598fcc79c7a2
Ajith P Poulose <ajith.poulose@bytestrone.com>
2025-02-10 13:40:43 +0530

    added asynchronous increment function in store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@
 
 import { useCounterStore } from "@/store/store";
 
+
 export default function Home() {
   const count = useCounterStore((state) => state.count);
   const increment = useCounterStore((state) => state.increment);
+  const incrementAsync = useCounterStore((state) => state.incrementAsync);
   const decrement = useCounterStore((state) => state.decrement);
   
   return (
     <div className="p-3 border flex flex-col">
       <div>{count}</div>
       <div>{count}</div>
-      <button onClick={increment}>Increment</button>
+      <button onClick={incrementAsync}>Increment</button>
       <button onClick={decrement}>Decrement</button>
     </div>
   );
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,13 +3,18 @@ import { create } from "zustand";
 interface CounterStore {
   count: number;
   increment: () => void;
+  incrementAsync: () => Promise<void>;
   decrement: () => void;
 }
 
 export const useCounterStore = create<CounterStore>((set) => ({
   count: 2,
-  increment: () => {
+  increment: () => {  
     set((state) => ({ count: state.count + 1 }));
+  },
+  incrementAsync : async ()=>{
+    await new Promise((resolve)=>setTimeout(resolve,2000));
+    set((state) => ({ count: state.count + 2 }));
   },
   decrement: () => {
     set((state) => ({ count: state.count - 1 }));
